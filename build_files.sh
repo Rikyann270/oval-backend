0f50207d0fd3b4a238be3ab9d926f32fa9197f92
Rikyann270 <rikyann270@gmail.com>
2024-03-19 10:41:19 -0700

    django downgrade

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
 echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
- sudo apt-get install build-dep python-psycopg2
+ python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
