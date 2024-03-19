0be3c4d433c4f818856591414242a34e936da46a
Rikyann270 <rikyann270@gmail.com>
2024-03-19 10:00:03 -0700

    psycop update2

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,4 @@
 echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
  sudo apt-get install build-dep python-psycopg2
- pip install psycopg2-binary
- python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
