b9262beba22ad18ad326b066c2952ca600f63a89
Rikyann270 <rikyann270@gmail.com>
2024-03-19 08:40:32 -0700

    sycop uddate

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,13 +113,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'nTKXMFORrgXIJaSlazQpfrIQbdNsFovp',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '15510',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'railway',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'nTKXMFORrgXIJaSlazQpfrIQbdNsFovp',
+    #     'HOST': 'roundhouse.proxy.rlwy.net',
+    #     'PORT': '15510',
+    # }
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
