c8e6f379a6e8c37af1b54a586cfcc70226b14dc8
Rikyann270 <rikyann270@gmail.com>
2024-03-21 07:47:58 -0700

    CSRF_TRUSTED_ORIGINS disable2

--- mysite/settings.py
+++ mysite/settings.py
@@ -85,7 +85,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173/",  
 ]
-CSRF_TRUSTED_ORIGINS = ['https://oval-backend-production.up.railway.app/', ]
+# CSRF_TRUSTED_ORIGINS = ['https://oval-backend-production.up.railway.app/', ]
 
 ROOT_URLCONF = 'mysite.urls'
 
