0857d84435f53509f85a560a955a96b3d709a863
Rikyann270 <rikyann270@gmail.com>
2024-03-21 08:16:30 -0700

    origin const

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,6 +33,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ['.vercel.app','now.sh']
+CSRF_TRUSTED_ORIGINS = ['https://oval-backend-production.up.railway.app', ]
 
 
 # Application definition
@@ -74,7 +75,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -85,7 +86,6 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173/",  
 ]
-# CSRF_TRUSTED_ORIGINS = ['https://oval-backend-production.up.railway.app/', ]
 
 ROOT_URLCONF = 'mysite.urls'
 
